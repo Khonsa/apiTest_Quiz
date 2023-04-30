d557aa86a00cc2bdfa4ef5d871496af2c4e35f8c
Khonsa <khonsaazizaalmash@gmail.com>
2023-05-01 02:31:22 +0700

    add OR post create auth token and edit get a booking data

--- Object Repository/GET/Get a booking data.rs
+++ Object Repository/GET/Get a booking data.rs
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <WebServiceRequestEntity>
    <description></description>
-   <name>Get a booking</name>
+   <name>Get a booking data</name>
    <tag></tag>
    <elementGuidId>6d9c1e22-aac0-4ba0-acb6-39431ad7bb51</elementGuidId>
    <selectorMethod>BASIC</selectorMethod>
@@ -12,11 +12,19 @@
    <httpBody></httpBody>
    <httpBodyContent></httpBodyContent>
    <httpBodyType></httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Accept</name>
+      <type>Main</type>
+      <value>application/json</value>
+      <webElementGuid>7685324b-555c-40a9-bdc9-fe0bfbc3948b</webElementGuid>
+   </httpHeaderProperties>
    <katalonVersion>8.6.0</katalonVersion>
    <maxResponseSize>0</maxResponseSize>
    <migratedVersion>5.4.1</migratedVersion>
    <restRequestMethod>GET</restRequestMethod>
-   <restUrl>https://restful-booker.herokuapp.com/booking/186</restUrl>
+   <restUrl>https://restful-booker.herokuapp.com/booking/1</restUrl>
    <serviceType>RESTful</serviceType>
    <soapBody></soapBody>
    <soapHeader></soapHeader>
@@ -37,6 +45,12 @@ import internal.GlobalVariable as GlobalVariable
 
 RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
 
-ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()</verificationScript>
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+
+WS.verifyResponseStatusCode(response, 200)
+
+assertThat(response.getStatusCode()).isEqualTo(200)
+
+assertThat(response.getStatusCode()).isIn(Arrays.asList(200, 201, 202))</verificationScript>
    <wsdlAddress></wsdlAddress>
 </WebServiceRequestEntity>
