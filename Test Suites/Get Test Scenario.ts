cd168ae73d4ea78203bff44cf3fb847dda7ab80c
Khonsa <khonsaazizaalmash@gmail.com>
2023-05-01 15:07:16 +0700

    add tc009-Get a booking data and add Test suite-Get test scenario

---
+++ Test Suites/Get Test Scenario.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Get Test Scenario</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>059a3ac1-ff0f-4a78-b34f-8c2993fdbe22</testSuiteGuid>
+   <testCaseLink>
+      <guid>8f9466f9-4569-4b79-89af-96c93c4a9f89</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SK01-Get Scenario/TC001-Validation response all booking id</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e3216417-5fff-4487-ac99-69d557265455</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SK01-Get Scenario/TC008-Validation response health check</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>691a3c63-87d3-4a04-b609-bb709f10c0eb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SK01-Get Scenario/TC009-Validation response get a booking data</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
